5bc72c56b958a4102afcecdf14f56b1ee2a43f8d
Gustavo Petruzzi <gustavo.petruzzi@softvision.com>
2022-12-03 20:23:11 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles.css';
+import logo from './logo.png';
+export const App = () => {
+  return (
+    <>
+      <img src={logo} alt="logo" width="300" height="200" />
+      <h1> React with typescript webpack template </h1>
+    </>
+  )
+}
