e2c49ce94451fb6105d7f13337597f3ceaf351ba
Gustavo Petruzzi <gustavo.petruzzi@softvision.com>
2022-12-03 22:13:50 -0300

    Add different enviroments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export const App = () => {
   return (
     <>
       <img src={logo} alt="logo" width="300" height="200" />
-      <h1> React with typescript webpack template </h1>
+      <h1> React with typescript webpack template - { process.env.NODE_ENV }</h1>
     </>
   )
 }
